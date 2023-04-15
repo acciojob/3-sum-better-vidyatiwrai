57f81f674cf1c91cb283b580468693e2a2957a2b
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-15 09:24:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,9 @@
   return nums[0] + nums[1] + nums[2];
 }
 
-console.log(sum_three([10, 32, 20]));  
-console.log(sum_three([5, 7, 9])); 
-console.log(sum_three([0, 8, -11]));
+// console.log(sum_three([10, 32, 20]));  
+// console.log(sum_three([5, 7, 9])); 
+// console.log(sum_three([0, 8, -11]));
 
 
 module.exports = threeSum;
